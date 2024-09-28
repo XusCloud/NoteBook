b4bd4046c09053c1358de22cc1c5e6e843db5441
XusCloud <XusCloud@gmail.com>
2024-09-28 15:15:54 +0800

    update: vim

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -1,7 +1,7 @@
 --[[
 lvim is the global options object
 
-Linters should be
+
 filled in as strings with either
 a global executable or a path to
 an executable
@@ -14,8 +14,9 @@ lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
 -- lvim.colorscheme = "lunar"
 -- lvim.colorscheme = "onedarker"
-lvim.colorscheme = "nightfox"
--- lvim.colorscheme = "vscode"
+-- lvim.colorscheme = "nightfox"
+lvim.colorscheme = "vscode"
+-- lvim.colorscheme = "kanagawa-wave"
 
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -47,6 +48,11 @@ keybinding_prefix["<C-p>"] = ":bprevious<CR>"
 
 vim.opt.relativenumber = true
 
+vim.cmd("let g:loaded_perl_provider = 0")
+vim.cmd("let g:python_host_prog = '/Users/aiden/.pyenv/shims/python'")
+vim.cmd("let g:python3_host_prog = '/opt/homebrew/bin/python3'")
+
+
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 -- local _, actions = pcall(require, "telescope.actions")
@@ -71,15 +77,38 @@ vim.opt.relativenumber = true
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
--- }
+-- lvim.builtin.which_key.setup.plugins.marks = true
+-- lvim.builtin.which_key.setup.plugins.registers = true
+lvim.builtin.which_key.setup.plugins.presets = {
+  operators = false,    -- adds help for operators like d, y, ...
+  motions = false,      -- adds help for motions
+  text_objects = false, -- help for text objects triggered after entering an operator
+  windows = false,      -- default bindings on <c-w>
+  nav = false,          -- misc bindings to work with windows
+  z = false,            -- bindings for folds, spelling and others prefixed with z
+  g = false,            -- bindings for prefixed with g
+}
+lvim.builtin.which_key.mappings.t = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+}
+
+lvim.builtin.which_key.mappings.S = {
+  name = "+Spectre",
+  s = { "<cmd>lua require('spectre').toggle()<CR>", "Toggle Spectre" },
+  w = { "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", "Search current word" },
+  p = { "<cmd>lua require('spectre').open_file_search({select_word=true})<CR>", "Search on current file" }
+}
+
+lvim.builtin.comment.active = false
+-- lvim.builtin.comment.opleader.line = 'g/'
+-- lvim.builtin.comment.opleader.block = "ga"
+
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -90,25 +119,26 @@ lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.view.width = 25
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 lvim.builtin.project.active = false
+lvim.builtin.lualine.options.theme = "vscode"
+
+lvim.builtin.cmp.formatting = {
+  format = require("tailwindcss-colorizer-cmp").formatter
+}
+
+-- for _, config in pairs(require("nvim-treesitter.parsers").get_parser_configs()) do
+--   config.install_info.url = config.install_info.url:gsub("https://github.com/", "something else")
+-- end
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+  "vim", "vimdoc", "query",
+  "comment", "markdown_inline", "regex",
+  "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "java", "yaml",
 }
 
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enable = true
+-- lvim.builtin.treesitter.ignore_install = { "haskell" }
+-- lvim.builtin.treesitter.rainbow.enable = true
+lvim.builtin.treesitter.context_commentstring = nil
 
 -- generic LSP settings
 
@@ -127,19 +157,21 @@ lvim.builtin.treesitter.highlight.enable = true
 -- }
 
 -- ---@usage disable automatic installation of servers
-lvim.lsp.installer.setup.automatic_installation = false
-lvim.lsp.automatic_servers_installation = true
+-- lvim.lsp.installer.setup.automatic_installation = true
+lvim.lsp.installer.setup.automatic_servers_installation = true
+-- lvim.lsp.automatic_servers_installation = true
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.offsetEncoding = { "utf-16" }
+require("lspconfig").clangd.setup({ capabilities = capabilities })
+
 
 -- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
 -- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
 -- require("lvim.lsp.manager").setup("pyright", opts)
-require("neoscroll").setup({
-  mappings = { "<C-d>", "<C-u>", "zz" },
-  respect_scrolloff = true,
-  easing_function = "quadratic"
-})
+
 
 local c = require('vscode.colors').get_colors()
 require('vscode').setup({
@@ -156,9 +188,27 @@ require('vscode').setup({
     -- this supports the same val table as vim.api.nvim_set_hl
     -- use colors from this colorscheme by requiring vscode.colors!
     Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+    -- NonText = { fg = (isDark and c.vscLineNumber or c.vscTabOther), bg = c.vscNone, bold = true },
+    EndOfBuffer = { fg = (c.vscCursorDarkDark or c.vscLeftDark), bg = c.vscNone },
+    -- Whitespace = { fg = isDark and c.vscLineNumber or c.vscTabOther },
   }
 })
-require('vscode').load()
+-- require('vscode').load()
+
+require("auto-save").setup {
+  trigger_events = {
+    "InsertLeave",
+    -- "TextChanged",
+    -- "TabClosed",
+    -- "WinClosed",
+    -- "TabLeave",
+    -- "WinLeave",
+    -- "CursorMoved",
+    -- "FocusLost",
+  },
+  debounce_delay = 5000
+}
+
 
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
 -- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
@@ -177,13 +227,17 @@ require('vscode').load()
 -- end
 
 local null_ls = require("null-ls")
-null_ls.setup({
+null_ls.setup {
   sources = {
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.code_actions.eslint_d,
+    -- null_ls.builtins.formatting.prettierd,
+    -- null_ls.builtins.formatting.prettier_eslint,
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.clang_format,
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.completion.spell,
-  },
-})
+  }
+}
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 -- local formatters = require "lvim.lsp.null-ls.formatters"
@@ -223,12 +277,74 @@ null_ls.setup({
 lvim.plugins = {
   { "lunarvim/darkplus.nvim" },
   { "lunarvim/horizon.nvim" },
-  -- { "lunarvim/Onedarker.nvim" },
   { "EdenEast/nightfox.nvim" },
+
   { "Mofiqul/vscode.nvim" },
 
-  --  { "neoclide/coc.nvim" },
-  { "karb94/neoscroll.nvim" },
+  {
+    "Exafunction/codeium.vim",
+    event = 'BufEnter',
+    config = function()
+      vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end
+  },
+
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require('kanagawa').setup({
+        compile = false,  -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false,   -- do not set background color
+        dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = {             -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function() -- add/modify highlights
+          return {}
+        end,
+        theme = "wave",  -- Load "wave" theme when 'background' option is not set
+        background = {   -- map the value of 'background' option to a theme
+          dark = "wave", -- try "dragon" !
+          light = "lotus"
+        },
+      })
+    end
+  },
+
+  -- { "neoclide/coc.nvim" },
+
+  {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+      require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { '<C-u>', '<C-d>',
+          'zt', 'zz', 'zb' },
+        hide_cursor = true,            -- Hide cursor while scrolling
+        stop_eof = true,               -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false,   -- Use the local scope of scrolloff instead of the global scope
+        respect_scrolloff = true,      -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true,   -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = "quadratic", -- Default easing function
+        pre_hook = nil,                -- Function to run before the scrolling animation starts
+        post_hook = nil,               -- Function to run after the scrolling animation ends
+      })
+    end
+  },
 
   {
     "folke/trouble.nvim",
@@ -237,12 +353,93 @@ lvim.plugins = {
   {
     "Pocco81/auto-save.nvim",
     config = function()
-      require("auto-save").setup({
-        trigger_events = { "InsertLeave" }
+    end,
+  },
+  {
+    'wfxr/minimap.vim',
+    build = "cargo install --locked code-minimap",
+    -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
+    config = function()
+      vim.cmd("let g:minimap_width = 10")
+      vim.cmd("let g:minimap_auto_start = 1")
+      vim.cmd("let g:minimap_auto_start_win_enter = 1")
+    end,
+  },
+  {
+    "kevinhwang91/nvim-bqf",
+    event = { "BufRead", "BufNew" },
+    config = function()
+      require("bqf").setup({
+        auto_enable = true,
+        preview = {
+          win_height = 12,
+          win_vheight = 12,
+          delay_syntax = 80,
+          border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+        },
+        func_map = {
+          vsplit = "",
+          ptogglemode = "z,",
+          stoggleup = "",
+        },
+        filter = {
+          fzf = {
+            action_for = { ["ctrl-s"] = "split" },
+            extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+          },
+        },
       })
     end,
   },
+  {
+    "windwp/nvim-spectre",
+    event = "BufRead",
+    config = function()
+      require("spectre").setup()
+    end,
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    event = "BufRead",
+    config = function()
+      require('ts_context_commentstring').setup {
+        enable_autocmd = false,
+      }
+    end
+  },
+  {
+    "mrjones2014/nvim-ts-rainbow",
+  },
+  {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    config = function()
+      require("tailwindcss-colorizer-cmp").setup({
+        color_square_width = 2,
+      })
+    end
+  },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   event = "InsertEnter",
+  --   dependencies = { "zbirenbaum/copilot.lua" },
+  --   config = function()
+  --     vim.defer_fn(function()
+  --       require("copilot").setup()   -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+  --       require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+  --     end, 100)
+  --   end,
+  -- }
+  --
 }
+
+
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- vim.api.nvim_create_autocmd("BufEnter", {
